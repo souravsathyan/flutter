7752f6953bc6edc6255eb738a31d768962381a49
souravsathyan <souravksathyan@gmail.com>
2025-04-14 12:59:08 +0530

    flutter expense tracker

---
+++ exp_tracker/lib/Modals/expense.dart
@@ -0,0 +1,5 @@
+class Expense {
+  Expense({
+    required this.title,
+  })
+}
---
+++ exp_tracker/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:exp_tracker/expenses.dart';
+
+void main() {
+  runApp(MaterialApp(home: Expenses()));
+}
